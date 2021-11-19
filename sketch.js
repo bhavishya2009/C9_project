72658b89891745f7984f3f6806ac64960fe337c1
bhavishya2009 <94637794+bhavishya2009@users.noreply.github.com>
2021-11-19 17:53:02 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30)
+}
+
+function draw() 
+{
+  background(30);
+  if (keyDown("right")){
+    box.position.x += 5;
+  }
+  if (keyDown("left")){
+    box.position.x -= 5;
+  }
+  if (keyDown("up")){
+    box.position.y -= 5;
+  }
+  if (keyDown("down")){
+    box.position.y += 5;
+  }
+  drawSprites();
+}
+
+
+
+
